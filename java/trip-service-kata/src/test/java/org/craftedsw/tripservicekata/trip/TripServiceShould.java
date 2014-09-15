ea17174b0097ccd6cec90db6162028ab0ca820ab
Amir <amiralibazazi@gmail.com>
2014-09-15 14:13:09 +0100

    tests were returning false positives due to a pointer error with the users (reusing the same users throughout the tests),now 3 tests are fixed, will branch off and start refactoring

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceShould.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceShould.java
@@ -2,10 +2,10 @@ package org.craftedsw.tripservicekata.trip;
 
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
-import org.craftedsw.tripservicekata.user.UserSession;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -13,32 +13,39 @@ import static org.junit.Assert.assertThat;
 
 public class TripServiceShould {
 
-    private static final User SOME_USER = new User();
+    private static final User UNUSED_USER = new User();
     private static final User GUEST = null;
     private User loggedUser;
     TripService tripService;
+    private User someUser;
+    private User userWithNoFriends;
 
     @Before
     public void initialise() {
+        userWithNoFriends = new User();
+        someUser = new User();
         loggedUser = new User();
         tripService = new TestableTripService();
     }
 
     @Test(expected= UserNotLoggedInException.class) public void
-	throw_an_exception_if_the_session_user_is_not_logged_in() {
+	throw_an_exception_if_the_user_is_not_logged_in() {
         loggedUser = GUEST;
-        tripService.getTripsByUser(SOME_USER);
+        tripService.getTripsByUser(UNUSED_USER);
 	}
 
     @Test public void
-    return_the_trips_of_a_friend_when_getTripsByUser_is_called() {
-        SOME_USER.addFriend(loggedUser);
-        SOME_USER.addTrip(new Trip());
-        assertThat(tripService.getTripsByUser(SOME_USER), is(SOME_USER.trips()));
+    return_the_trips_of_a_friend() {
+        someUser.addFriend(loggedUser);
+        someUser.addTrip(new Trip());
+        assertThat(tripService.getTripsByUser(someUser), is(someUser.trips()));
     }
 
     @Test public void
-    return_null_if_getTripsByUser_is_called_on_a_user_that_is_not_a_friend() {
+    return_nothing_if_the_trips_of_a_stranger_is_requested() {
+        List<Trip> emptyList = new ArrayList<Trip>();
+        userWithNoFriends.addTrip(new Trip());
+        assertThat(tripService.getTripsByUser(userWithNoFriends), is(emptyList));
     }
 
     private class TestableTripService extends TripService {
