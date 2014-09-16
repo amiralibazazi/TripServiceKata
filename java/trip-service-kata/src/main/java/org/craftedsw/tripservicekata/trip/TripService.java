7f8666873844469c1db218b1eef2cf7ca63a04d3
Amir <amiralibazazi@gmail.com>
2014-09-16 10:20:02 +0100

    new branch

--- java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -8,11 +8,11 @@ import org.craftedsw.tripservicekata.user.User;
 import org.craftedsw.tripservicekata.user.UserSession;
 
 public class TripService {
+
+	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
 		List<Trip> tripList = new ArrayList<Trip>();
 		User loggedUser = UserSession.getInstance().getLoggedUser();
 		boolean isFriend = false;
-
-	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
 		if (loggedUser != null) {
 			for (User friend : user.getFriends()) {
 				if (friend.equals(loggedUser)) {
--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceShould.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceShould.java
@@ -2,8 +2,10 @@ package org.craftedsw.tripservicekata.trip;
 
 import org.junit.Test;
 
-public class TripServiceShould {    
+public class TripServiceShould {
+
     @Test public void
-    throw_an_exception_if_no_one_is_logged_in() {
+    validate_a_logged_in_user() {
+
     } 
 }
